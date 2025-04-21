506b609b6636dc5090f653bbf8d5f93f2161857e
popsiclexu <ZhenxueXu@gmail.com>
2025-04-21 23:29:17 +0800

    feat: add 'customrest' to backend options in CRD (#666)
    Signed-off-by: popsiclexu <ZhenxueXu@gmail.com>
    Co-authored-by: Alex Jones <alexsimonjones@gmail.com>

--- api/v1alpha1/k8sgpt_types.go
+++ api/v1alpha1/k8sgpt_types.go
@@ -111,7 +111,7 @@ type AutoRemediation struct {
 type AISpec struct {
 	AutoRemediation AutoRemediation `json:"autoRemediation,omitempty"`
 	// +kubebuilder:default:=openai
-	// +kubebuilder:validation:Enum=ibmwatsonxai;openai;localai;azureopenai;amazonbedrock;cohere;amazonsagemaker;google;googlevertexai
+	// +kubebuilder:validation:Enum=ibmwatsonxai;openai;localai;azureopenai;amazonbedrock;cohere;amazonsagemaker;google;googlevertexai;customrest
 	Backend string   `json:"backend"`
 	BackOff *BackOff `json:"backOff,omitempty"`
 	BaseUrl string   `json:"baseUrl,omitempty"`
